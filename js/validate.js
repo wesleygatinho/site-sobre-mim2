767d37a3bebb565e0852f6bcafa15cd9b24c242d
wesleygatinho <wesley.gatinho@discente.ufma.br>
2024-05-24 00:03:24 -0300

    Correção código

--- js/validate.js
+++ js/validate.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 // <!-- Menu Hambuguer - Tela Mobile  -->
 function animar()  {
@@ -21,27 +21,3 @@ btnMenu.addEventListener('click', () =>{
   btnClicado = !btnClicado;
   
 });
-=======
-
-// <!-- Menu Hambuguer - Tela Mobile  -->
-function animar()  {
-  const btn = document.getElementById('btn-menu');
-  btn.classList.toggle('ativar');
-
-}
-
-
-// <!-- Esconder e Ativar Menu - Tela Mobile  -->
-const btnMenu = document.getElementById('btn-menu');
-const menuList = document.querySelector('.menu__list');
-
-let btnClicado = false;
-if(btnClicado){
-  alert('clicado');
-}
-btnMenu.addEventListener('click', () =>{
-  btnClicado ? menuList.classList.add('invisivel') : menuList.classList.remove('invisivel');
-  btnClicado = !btnClicado;
-  
-});
->>>>>>> fe24ac125a6a71f65feef3a3fd7277fbab5e7242
